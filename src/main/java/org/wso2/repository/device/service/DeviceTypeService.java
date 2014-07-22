2382ab0d1f3fccbbf38bcf7246ffc44d7614a2a0
undefined undefined <liyanagemanil@gmail.com>
2014-07-21 21:23:20 -0700

    ddd

--- src/main/java/org/wso2/repository/device/service/DeviceTypeService.java
+++ src/main/java/org/wso2/repository/device/service/DeviceTypeService.java
@@ -1,5 +1,8 @@
 package org.wso2.repository.device.service;
 
+import demo.jaxrs.server.Customer;
+import demo.jaxrs.server.Order;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -9,25 +12,132 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 import javax.ws.rs.Path;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+
 @Path("/devicerepository/")
 public class DeviceTypeService
 {
-    public DeviceTypeService(){
+    long currentId = 123;
+    Map<Long, Customer> customers = new HashMap<Long, Customer>();
+    Map<Long, Order> orders = new HashMap<Long, Order>();
+
+    public DeviceTypeService() {
+        init();
+    }
+
+    @GET
+    @Path("/customers/{id}/")
+    public Customer getCustomer(@PathParam("id") String id) {
+        System.out.println("----invoking getCustomer, Customer id is: " + id);
+        long idNumber = Long.parseLong(id);
+        Customer c = customers.get(idNumber);
+        return c;
+    }
+
+    @PUT
+    @Path("/customers/")
+    public Response updateCustomer(Customer customer) {
+        System.out.println("----invoking updateCustomer, Customer name is: " + customer.getName());
+        Customer c = customers.get(customer.getId());
+        Response r;
+        if (c != null) {
+            customers.put(customer.getId(), customer);
+            r = Response.ok().build();
+        } else {
+            r = Response.notModified().build();
+        }
+
+        return r;
+    }
+
+    @POST
+    @Path("/customers/")
+    public Response addCustomer(Customer customer) {
+        System.out.println("----invoking addCustomer, Customer name is: " + customer.getName());
+        customer.setId(++currentId);
+
+        customers.put(customer.getId(), customer);
+
+        return Response.ok(customer).build();
+    }
+
+    // Adding a new method to demonstrate Consuming and Producing text/plain
+
+    @POST
+    @Path("/customers/name/")
+    @Consumes("text/plain")
+    @Produces("text/plain")
+    public String getCustomerName(String id) {
+        System.out.println("----invoking getCustomerName, Customer id is: " + id);
+        return "Isuru Suriarachchi";
+    }
+
+    @DELETE
+    @Path("/customers/{id}/")
+    public Response deleteCustomer(@PathParam("id") String id) {
+        System.out.println("----invoking deleteCustomer, Customer id is: " + id);
+        long idNumber = Long.parseLong(id);
+        Customer c = customers.get(idNumber);
+
+        Response r;
+        if (c != null) {
+            r = Response.ok().build();
+            customers.remove(idNumber);
+        } else {
+            r = Response.notModified().build();
+        }
+
+        return r;
+    }
+
+    @Path("/orders/{orderId}/")
+    public Order getOrder(@PathParam("orderId") String orderId) {
+        System.out.println("----invoking getOrder, Order id is: " + orderId);
+        long idNumber = Long.parseLong(orderId);
+        Order c = orders.get(idNumber);
+        return c;
+    }
+
+    final void init() {
+        Customer c = new Customer();
+        c.setName("John");
+        c.setId(123);
+        customers.put(c.getId(), c);
+
+        Order o = new Order();
+        o.setDescription("order 223");
+        o.setId(223);
+        orders.put(o.getId(), o);
         
-       /* try
+        
+        try
         {
         DataSource dataSource = null;
     InitialContext context = new InitialContext();
-    dataSource = (DataSource) context.lookup("jdbc/DeviceMgt");
+    dataSource = (DataSource) context.lookup("jdbc/DeviceNew");
     
 			Connection con= dataSource.getConnection();
 			Statement statement = con.createStatement();
-			String query = "select * from devmgt_isg9251.device";
+			String query = "select * from test2_manil8056.device";
 			ResultSet resultSet = statement.executeQuery(query);
         }
         catch (Exception e)
         {
             System.out.println(e.toString());
-        }*/
+         }
+        
     }
+
 }

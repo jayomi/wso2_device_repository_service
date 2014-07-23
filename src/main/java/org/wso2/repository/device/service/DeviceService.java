a9b26657e7660f510d6f796efa3973203bd41b35
jayomi <jayomi.rajapaksha@gmail.com>
2014-07-23 12:10:40 +0530

    aaa

--- src/main/java/org/wso2/repository/device/service/DeviceService.java
+++ src/main/java/org/wso2/repository/device/service/DeviceService.java
@@ -4,11 +4,9 @@ import org.wso2.repository.device.data.Device;
 
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -27,7 +25,7 @@ public class DeviceService
 
 
 
-/*
+
 
     @DELETE
     @Path("/deletedevice/{id}/")
@@ -58,7 +56,7 @@ public class DeviceService
 
 
     }
-*/
+
 
    @GET
    @Path("/getdevice/{id}/")

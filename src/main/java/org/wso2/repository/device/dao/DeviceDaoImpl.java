5d73398abffc4e8b8bfc335a54e7acb08ea9ea54
Manil Liyanage <liyanagemanil@gmail.com>
2014-08-03 20:51:53 +0530

    device

--- src/main/java/org/wso2/repository/device/dao/DeviceDaoImpl.java
+++ src/main/java/org/wso2/repository/device/dao/DeviceDaoImpl.java
@@ -174,6 +174,101 @@ public class DeviceDaoImpl implements DeviceDao{
 
     }
 
+///Detailed Service
+    public LinkedList<Device> getDevicesDetail(UriInfo parameters) {
+
+        String strResponse="";
+        LinkedList deviceList=new LinkedList();
+
+        try {
+            String deviceId = parameters.getQueryParameters().getFirst("deviceId");
+            String deviceName = parameters.getQueryParameters().getFirst("deviceName");
+            String statusId = parameters.getQueryParameters().getFirst("statusId");
+            String typeId = parameters.getQueryParameters().getFirst("typeId");
+            String options = null;
+
+            Connection con=DB.getConnection();
+            Statement statement=con.createStatement();
+            String query ="Select d_id , d_name, d_description,  status as s_id , type as t_id " +
+                    " from device d " +
+                    " left join status s on s.s_id = d.s_id " +
+                    " left join device_type dt on dt.t_id = d.t_id";
+
+            boolean firstPara = false;
+
+            if (deviceId !=null)
+            {
+                options = " d.d_id = '" + deviceId +"' ";
+                firstPara =true;
+            }
+
+            if (deviceName !=null)
+            {
+                if (firstPara==false) {
+                    options = " d.d_name = '" + deviceName+ "' ";
+                    firstPara = true;
+                }else
+                {
+                    options = options +  " AND d.d_name = '" + deviceName + "' ";
+                }
+
+            }
+
+            if (statusId !=null)
+            {
+                if (firstPara==false) {
+                    options = " s.status = '" + statusId + "' ";
+                    firstPara = true;
+                }else
+                {
+                    options = options +  " AND s.status = '" + statusId + "' ";
+                }
+
+            }
+            if (typeId !=null)
+            {
+                if (firstPara==false) {
+                    options = " dt.type = '" + typeId + "' ";
+                    firstPara = true;
+                }else
+                {
+                    options = options +  " AND dt.type = '" + typeId + "' ";
+                }
+
+            }
+            if(firstPara)
+            {
+                query = query + " Where " + options;
+            }
+
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+
+                Device device=new Device();
+                device.setDeviceId(resultSet.getString("d_id"));
+                device.setDeviceName(resultSet.getString("d_name"));
+                device.setDeviceDescription(resultSet.getString("d_description"));
+                device.setStatusId(resultSet.getString("s_id"));
+                device.setTypeId(resultSet.getString("t_id"));
+                deviceList.add(device);
+            }
+
+            strResponse="Ok,Executed the Query";
+            System.out.println( strResponse);
+            return deviceList;
+
+        }catch (Exception e) {
+            e.printStackTrace();
+            strResponse="Data Not Executed";
+            System.out.println( strResponse);
+            return deviceList;
+
+        }finally{
+            return deviceList;
+        }
+
+    }
+
     //update a device
     public String updateDevice(Device device,String id) {
 
